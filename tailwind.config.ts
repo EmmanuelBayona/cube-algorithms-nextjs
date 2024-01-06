c53635e1b53f3af8034e337e13682928c4da0304
Emmanuel <bayonaj368@gmail.com>
2024-01-05 22:41:55 -0600

    style: provisional title

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			backgroundImage: {
+				'purple-gradient': 'radial-gradient(40% 100% at 50% 0%, #15082B 0%, #000 100%)',
+			},
+			dropShadow: {
+				// 'text': '0 2px 16px rgba(174,207,242, 0.25)',
+				// 'text': '0 2px 16px rgba(196, 174, 242, 0.4)',
+				'text': '0 2px 16px rgba(169, 151, 200, 0.45)',
+			}
+		},
+	},
+	plugins: [],
 }
 export default config
