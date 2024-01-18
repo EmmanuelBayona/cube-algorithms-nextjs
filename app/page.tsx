f98c7023c114f592d363d7851872dec1f096da1a
Emmanuel <bayonaj368@gmail.com>
2024-01-18 01:47:59 -0600

    Update MaxWidthWrapper styling in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     return (
         <div className="relative min-h-screen w-full bg-dark">
             <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] pointer-events-none" aria-hidden='true' />
-            <MaxWidthWrapper className="text-violet-100 pt-5 relative z-10">
+            <MaxWidthWrapper className="text-violet-100 pt-5 relative">
                 <LandingNavbar />
                 <h1
                     className="mt-10 text-center text-6xl md:text-8xl lg:text-9xl font-semibold drop-shadow-text tracking-tight"
