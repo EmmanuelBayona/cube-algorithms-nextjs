57b1134d9fb90003a30f99305537b4c9a4fdeb5a
Emmanuel <bayonaj368@gmail.com>
2024-02-26 23:21:18 -0600

    reafactor: pass forms logic to custom hook

---
+++ hooks/use-new-method.ts
@@ -0,0 +1,60 @@
+import { FormEvent, useState } from "react";
+import { DBCubes, DBMethods } from "@/types"
+import { showToastError, showToastSuccess } from "@/lib/toaster";
+import { addNewMethod } from "@/actions";
+
+
+export const useNewMethod = ({ cubes, methods }: { cubes: DBCubes[], methods: DBMethods[] }) => {
+
+    const [cube, setCube] = useState<string>('');
+    const [method, setMethod] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+    const [status, setStatus] = useState<'idle' | 'loading' | 'error' | 'success'>('idle');
+
+    const onAddNewMethod = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (status === 'loading') return;
+
+        if (!cube) return showToastError('Cube is required');
+        if (!method) return showToastError('Method name is required');
+        if (!description) return showToastError('Method description is required');
+
+        // if the method already exists, return an error 
+        if (methods.find(m => m.name === method)) return showToastError('Method already exists');
+
+        setStatus('loading')
+
+        const cubeId = cubes.find(c => c.name === cube)?.id;
+        if (!cubeId) {
+            showToastError('Something went wrong');
+            setStatus('error');
+            return;
+        }
+
+        const { success } = await addNewMethod(method, description, cubeId);
+
+        if (!success) {
+            showToastError('Something went wrong');
+            setStatus('error');
+            return;
+        }
+
+        setCube('');
+        setMethod('');
+        setDescription('');
+        showToastSuccess('Cube added successfully');
+        setStatus('success');
+    }
+
+    return {
+        cube,
+        setCube,
+        method,
+        setMethod,
+        description,
+        setDescription,
+        status,
+        onAddNewMethod
+    }
+
+}
