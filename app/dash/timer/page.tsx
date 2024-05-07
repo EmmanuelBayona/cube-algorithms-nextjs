5140222f681dc3dc9b7dd68673445d23f736751b
Emmanuel <bayonaj368@gmail.com>
2024-05-06 23:49:39 -0600

    feat: generate scramble

--- app/dash/timer/page.tsx
+++ app/dash/timer/page.tsx
@@ -1,6 +1,7 @@
 import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 import { AverageTimes } from "@/components/timer/average-times";
 import { BestTime } from "@/components/timer/best-time";
+import { Scramble } from "@/components/timer/scramble";
 import { TimerClock } from "@/components/timer/timer-clock";
 import { Times } from "@/components/timer/times";
 import { TotalSolves } from "@/components/timer/total-solves";
@@ -17,7 +18,7 @@ export default function Timer() {
                 <TotalSolves />
             </section>
 
-            {/* <Scramble /> */}
+            <Scramble />
             <TimerClock />
 
             <section className="mt-5 grid grid-cols-1 lg:grid-cols-3 gap-5">
