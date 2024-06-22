4edf568a0d71d5196d163a89b285de08610126d8
Emmanuel <bayonaj368@gmail.com>
2024-06-21 19:49:24 -0600

    chore: delete console.log

--- components/timer/times-plot.tsx
+++ components/timer/times-plot.tsx
@@ -16,8 +16,6 @@ export const TimesPlot = () => {
         times: 'bg-blue-500',
     };
 
-    console.log(timesWithNumeration)
-
     return (
         <Card className="w-full lg:w-72 h-[320px] flex items-center px-4">
             <AreaChart
